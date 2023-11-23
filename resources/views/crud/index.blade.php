1759005b6dfbc9879343805dcde2b4ce4b1a27e7
Alethepata <alessia.patane@gmail.com>
2023-11-23 16:20:15 +0100

    [Add] card

--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.main')
 
 @section('content')
+
     <h1>Fumetti</h1>
+
+    <a href="">
+        <div class="card_container d-flex flex-wrap justify-content-center">
+            @foreach ($comics as $comic)
+                <div class="card mx-3 my-4">
+                    <img src="{{$comic->thumb}}" alt="">
+                </div>
+            @endforeach
+        </div>
+    </a>
+
 @endsection
