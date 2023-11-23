9ff45cacdacba8e3512f278d284702b88accbc73
Alethepata <alessia.patane@gmail.com>
2023-11-23 16:03:18 +0100

    [Add] topbar

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="topbar mt-3">
+        <nav>
+            <ul class="d-flex list-unstyled">
+                <li>
+                    <a href="{{route('home')}}">Home</a>
+                </li>
+                <li>
+                    <a href="{{route('comics.index')}}">Fumetti</a>
+                </li>
+                <li>
+                    <a href="{{route('home')}}">Aggiungi un fumetto</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
